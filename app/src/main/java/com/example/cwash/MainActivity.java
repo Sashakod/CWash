6d98011db71e9240e00279f72f34c2c8a7c9d159
Пользователь <sashakod@gmail.com>
2016-03-15 18:09:01 +0300

    Initial commit

---
+++ app/src/main/java/com/example/cwash/MainActivity.java
@@ -0,0 +1,130 @@
+package com.example.cwash;
+
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.cwash.fragment.AccManagerFragment;
+import com.example.cwash.fragment.ExitFragment;
+import com.example.cwash.fragment.HistoryFragment;
+import com.example.cwash.fragment.SettingsFragment;
+import com.example.cwash.fragment.StaticsFragment;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    FragmentTransaction fragmentTransaction;
+    Fragment historyFragment;
+    Fragment staticsFragment;
+    Fragment accManagerFragment;
+    Fragment settingsFragment;
+    Fragment exitFragment;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        historyFragment = new HistoryFragment();
+        staticsFragment = new StaticsFragment();
+        accManagerFragment = new AccManagerFragment();
+        settingsFragment = new SettingsFragment();
+        exitFragment = new ExitFragment();
+
+
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        fragmentTransaction = getFragmentManager().beginTransaction();
+        switch(item.getItemId()){
+            case R.id.nav_history:
+                fragmentTransaction.replace(R.id.fragment_container, historyFragment);
+                break;
+            case R.id.nav_statistics:
+                fragmentTransaction.replace(R.id.fragment_container, staticsFragment);
+                break;
+            case R.id.nav_acc_manager:
+                fragmentTransaction.replace(R.id.fragment_container, accManagerFragment);
+                break;
+            case R.id.nav_settings:
+                fragmentTransaction.replace(R.id.fragment_container, settingsFragment);
+                break;
+            case R.id.nav_exit:
+                fragmentTransaction.replace(R.id.fragment_container, exitFragment);
+                break;
+        }
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
---
+++ app/src/main/java/com/example/cwash/fragment/HistoryFragment.java
@@ -0,0 +1,19 @@
+package com.example.cwash.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.cwash.R;
+
+public class HistoryFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_history,null);
+        return v;
+    }
+}
